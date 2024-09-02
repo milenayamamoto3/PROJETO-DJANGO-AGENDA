a90e2e266d94cc9198f18f26a25b5ee69cdf5bab
Milena Yamamoto <milymanamy3@gmail.com>
2024-09-02 19:45:44 +0900

    Creating the category model and foreign key

--- contact/models.py
+++ contact/models.py
@@ -15,7 +15,12 @@ from django.utils import timezone
 #Por último
 # owner (foreign key)
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.name
+    
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -25,6 +30,11 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
