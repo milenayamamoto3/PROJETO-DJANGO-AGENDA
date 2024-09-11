ce4b43a67f5a23165603cf1915800a81158dcfc3
Milena Yamamoto <milymanamy3@gmail.com>
2024-09-11 16:13:14 +0900

    Handling Django QuerySets with filter, order_by and slicing

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[0:20]
+    
+    # print(contacts.query) #consulta SQL na tabela
 
     context = {
         'contacts': contacts
