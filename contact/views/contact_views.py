158cff320cd5d3c48c26ffef561a77285511eb7c
Milena Yamamoto <milymanamy3@gmail.com>
2024-09-16 17:29:41 +0900

    Filtering contacts using "Q" and "or"

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.db.models import Q
 from contact.models import Contact
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
 def index(request):
     contacts = Contact.objects \
@@ -12,6 +13,37 @@ def index(request):
     context = {
         'contacts': contacts,
         'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    print('search_value:', search_value)
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+    
+    # print(contacts.query) #consulta SQL na tabela
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
     }
 
     return render(
