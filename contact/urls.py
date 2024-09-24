ff26c298af8ae3127f5a77317733d248b036170b
Milena Yamamoto <milymanamy3@gmail.com>
2024-09-24 15:29:49 +0900

    Initializing the template, view and url to create a contact

--- contact/urls.py
+++ contact/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     # contact (CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'),
-]
+    path('contact/create/', views.create, name='create'),
+]   
