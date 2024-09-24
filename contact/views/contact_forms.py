3bf37d5fa9e39eb998995fb35d444bb6ac57e46c
Milena Yamamoto <milymanamy3@gmail.com>
2024-09-24 16:24:54 +0900

    Using request.method and request.POST to know the method and form information

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -6,12 +6,17 @@ from contact.models import Contact
 
 
 def create(request):
-    post = request.POST
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
     
     context = {
 
     }
-
+    
     return render(
         request,
         'contact/create.html',
