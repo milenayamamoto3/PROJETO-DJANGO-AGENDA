f3c18b6e7a669f17dd41577968476876575a8175
Milena Yamamoto <milymanamy3@gmail.com>
2024-09-25 16:01:40 +0900

    Validating form fields with clean using the "add_error" method

--- contact/forms.py
+++ contact/forms.py
@@ -38,19 +38,30 @@ class ContactForm(forms.ModelForm):
         # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
                 code='invalid'
             )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+        
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+        
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Não digite "ABC"',
+                    code='invalid'
+                )
+            )
+
+        return first_name
