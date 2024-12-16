082969ee5d37f33feb94e634c46e61bb1712bd92
Milena Yamamoto <milymanamy3@gmail.com>
2024-12-16 19:46:21 +0900

    Working with ImageField in the template and forms

--- contact/forms.py
+++ contact/forms.py
@@ -3,19 +3,27 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-                'class': 'classe-a classe-b',
-                'placeholder': 'Aqui veio do init',
+                'accept': 'image/*',
             }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário',
+        )
     )
+    
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'class': 'classe-a classe-b',
+    #             'placeholder': 'Aqui veio do init',
+    #         }
+    #     ),
+    #     label='Primeiro Nome',
+    #     help_text='Texto de ajuda para seu usuário',
+    # )
         
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
         # Atualizando o widget
         # self.fields['first_name'].widget.attrs.update({
         #     'class': 'classe-a classe-b',
@@ -27,6 +35,7 @@ class ContactForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
         )
         # Criando um widget
         # widgets = {
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -7,7 +7,7 @@ def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
     #     print()
     #     print(request.method)
     #     print(request.POST.get('first_name'))
@@ -45,7 +45,7 @@ def update(request, contact_id):
     )
     form_action = reverse('contact:update', args=(contact_id,))
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
         context = {
             'form': form,
             'form_action': form_action,
