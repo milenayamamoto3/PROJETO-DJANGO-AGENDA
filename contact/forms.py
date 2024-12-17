f0f834083707713eb9bd4cf102e1fadb0bb0f2bb
Milena Yamamoto <milymanamy3@gmail.com>
2024-12-17 13:24:13 +0900

    Using UserCreationForm to create new users in Django

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 class ContactForm(forms.ModelForm):
+
     picture = forms.ImageField(
         widget=forms.FileInput(
             attrs={
@@ -75,3 +77,6 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
+    
+class RegisterForm(UserCreationForm):
+    ...
