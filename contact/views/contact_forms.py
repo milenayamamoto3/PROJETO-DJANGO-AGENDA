9ae5ea6198716ceb90f7d6fcb0fb0fdb10176080
Milena Yamamoto <milymanamy3@gmail.com>
2024-12-17 21:36:21 +0900

    Creating the relationship between contact and owner (request.user)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -21,7 +21,9 @@ def create(request):
         }
         
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
     
         return render(
@@ -44,7 +46,7 @@ def create(request):
 @login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     form_action = reverse('contact:update', args=(contact_id,))
     if request.method == 'POST':
@@ -56,7 +58,7 @@ def update(request, contact_id):
 
         if form.is_valid():
             contact = form.save()
-            return redirect('contact:update', contact_id=contact.pk)
+            return redirect('contact:contact', contact_id=contact.pk)
         
         return render(
             request,
@@ -78,7 +80,7 @@ def update(request, contact_id):
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     confirmation = request.POST.get('confirmation', 'no')
 
